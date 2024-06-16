44e651f830aa02d92659fde20a51a6ba6a1bbbb8
ichi <youduotian64@gmail.com>
2024-06-16 13:20:37 +0900

    3D玉転がしの出来上がり

---
+++ 3DRollBall/Assets/Scripts/PlayerConttoller.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerConttoller : MonoBehaviour
+{
+
+    Rigidbody  rb;
+    public float speed;
+    int count;
+    public Text countText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float H = Input.GetAxis("Horizontal");
+        float V = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(H,0,V);
+        rb.AddForce(move*speed);
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        other.gameObject.SetActive(false);
+        count += 1;
+        Debug.Log(count);
+        SetCountText();
+    }
+
+    void SetCountText()
+    {
+        countText.text = "獲得文字数：" + count.ToString();
+    }
+}
