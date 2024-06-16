abcc56468e1b01c578ba91f5be63e204ca8a08d2
ichi <youduotian64@gmail.com>
2024-06-16 13:53:40 +0900

    3D玉転がしその２の出来上がり

---
+++ 3DRollBall - コピー(別ゲー)/Assets/Scripts/GoalChecker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalChecker : MonoBehaviour
+{
+    public AudioSource gameBgm;
+    public AudioSource goalBgm;
+    public GameObject retryButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        retryButton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+
+        gameBgm.Stop();
+        goalBgm.Play();
+        retryButton.SetActive(true);
+    }
+
+    public void RetryStage()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
